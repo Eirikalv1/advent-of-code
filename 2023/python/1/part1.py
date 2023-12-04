0520d983d90a5b8b9029c154295c01d1c6bc8477
Eirik <eirikalv@icloud.com>
2023-12-04 16:13:15 +0000

    refactored

--- 2023/python/1/part1.py
+++ 2023/python/1/part1.py
@@ -1,8 +1,5 @@
 import re
 
 lines = open("input.txt", "r").readlines()
-num = 0
-for line in lines:
-    num += int(re.search(r"\d", line).group() \
-        + re.search(r"\d", line[::-1]).group())
-print(num)
+print(sum([int(re.search(r"\d", line).group() \
+    + re.search(r"\d", line[::-1]).group()) for line in lines]))
