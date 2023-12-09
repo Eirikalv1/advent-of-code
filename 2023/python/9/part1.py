29a0478ad7dfa0b05a25a8e9cfbb16b91d001e7b
Eirik <eirikalv@icloud.com>
2023-12-09 10:51:09 +0100

    small refactoring

--- 2023/python/9/part1.py
+++ 2023/python/9/part1.py
@@ -1,20 +1,13 @@
-def getDiffs(diffs):
-    while any(diffs[-1]) != 0:
-        diffs.append([b - a for a, b in zip(diffs[-1][:-1], diffs[-1][1:])])
-    return diffs
-
-def reverse(diffs):
-    diffs = diffs[::-1]
-    diffs[0].append(0)
-    for i, diff in enumerate(diffs[1:]):
-        diffs[i+1].append(diff[-1] + diffs[i][-1])
-    return diffs
-
 lines = open("input.txt", "r").readlines()
 
 total = 0
 for line in lines:
-    line = [list(map(int, line.strip().split()))]
-    diffs = reverse(getDiffs(line))
+    diffs = [list(map(int, line.strip().split()))]
+    while any(diffs[-1]) != 0:
+        diffs.append([b - a for a, b in zip(diffs[-1][:-1], diffs[-1][1:])])
+
+    diffs = diffs[::-1]
+    for i, diff in enumerate(diffs[1:]):
+        diffs[i+1].append(diff[-1] + diffs[i][-1])
     total += diffs[-1][-1]
 print(total)
