95ccf4323796e4e5146a685f6de901a9b3aaac54
Eirik <eirikalv@icloud.com>
2023-12-10 15:57:24 +0100

    refactored

--- 2023/python/10/part1.py
+++ 2023/python/10/part1.py
@@ -1,59 +1,28 @@
-lines = open("input.txt", "r").read().splitlines()
-lines.append("." * len(lines[0]))
-lines.insert(0, "." * len(lines[0]))
-for i in range(len(lines)): 
-    lines[i] = "." + lines[i] + "."
+GRID = open("input.txt", "r").readlines()
+
+DIRS = {
+    "S": [(1, 0), (-1, 0), (0, 1), (0, -1)], ".": [],
+    "-": [(1, 0), (-1, 0)], "|": [(0, 1), (0, -1)],
+    "F": [(1, 0), (0, 1)], "L": [(1, 0), (0, -1)],
+    "7": [(-1, 0), (0, 1)], "J": [(-1, 0), (0, -1)],
+}
+
+sym = lambda x, y: GRID[y][x] 
+
+def move(path):
+    x, y = path[-1][0], path[-1][1]
+    for d in DIRS[sym(x, y)]:
+        if (-d[0], -d[1]) in DIRS[sym(x+d[0], y+d[1])] \
+            and not (x+d[0], y+d[1]) in path:
+                return x+d[0], y+d[1]
 
 path = []
+for y in range(len(GRID)):
+    for x in range(len(GRID[y])):
+        if sym(x, y) == "S": path.append((x, y))
 
-def getStart():
-    for line in range(len(lines)):
-        for char in range(len(lines[line])):
-            if lines[line][char] == "S": return [char, line, "S"]
-
-def move(current):
-    dirs = []
-    match current[2]:
-        case "S": dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        case "-": dirs = [[1, 0], [-1, 0]]
-        case "|": dirs = [[0, 1], [0, -1]]
-        case "F": dirs = [[1, 0], [0, 1]]
-        case "J": dirs = [[-1, 0], [0, -1]]
-        case "L": dirs = [[1, 0], [0, -1]]
-        case "7": dirs = [[-1, 0], [0, 1]]
-    for dir in dirs:
-        if direction(current, dir):
-            return direction(current, dir)
-
-def direction(current, dir):
-    moves = {
-        "right": ["7", "J", "-", "S"],
-        "left": ["F", "L", "-", "S"],
-        "down": ["L", "J", "|", "S"],
-        "up": ["F", "7", "|", "S"]}
-    x, y = current[0], current[1]
-    match dir:
-        case [1, 0]:
-            pos = lines[y][x+1]
-            if pos in moves["right"] and not [x+1, y, pos] in path:
-                return [x+1, y, pos]
-        case [-1, 0]:
-            pos = lines[y][x-1]
-            if pos in moves["left"] and not [x-1, y, pos] in path:
-                return [x-1, y, pos]
-        case [0, 1]:
-            pos = lines[y+1][x]
-            if pos in moves["down"] and not [x, y+1, pos] in path:
-                return [x, y+1, pos]
-        case [0, -1]:
-            pos = lines[y-1][x]
-            if pos in moves["up"] and not [x, y-1, pos] in path:
-                return [x, y-1, pos]
-
-current = getStart()
-path.append(current)
 while True:
-    current = move(current)
-    if current == None: break
-    path.append(current)
+    step = move(path)
+    if step == None: break
+    path.append(step)
 print(len(path) / 2)
--- 2023/python/10/part2.py
+++ 2023/python/10/part2.py
@@ -1,77 +1,37 @@
-from shapely.geometry import Polygon, Point 
+GRID = open("input.txt", "r").readlines()
 
-lines = open("input.txt", "r").read().splitlines()
+DIRS = {
+    "S": [(1, 0), (-1, 0), (0, 1), (0, -1)], ".": [],
+    "-": [(1, 0), (-1, 0)], "|": [(0, 1), (0, -1)],
+    "F": [(1, 0), (0, 1)], "L": [(1, 0), (0, -1)],
+    "7": [(-1, 0), (0, 1)], "J": [(-1, 0), (0, -1)],
+}
 
-lines.append("." * len(lines[0]))
-lines.insert(0, "." * len(lines[0]))
-for i in range(len(lines)): 
-    lines[i] = "." + lines[i] + "."
+sym = lambda x, y: GRID[y][x] 
 
+def move(path):
+    x, y = path[-1][0], path[-1][1]
+    for d in DIRS[sym(x, y)]:
+        if (-d[0], -d[1]) in DIRS[sym(x+d[0], y+d[1])] \
+            and not (x+d[0], y+d[1]) in path:
+                return x+d[0], y+d[1]
 
 path = []
-def getStart():
-    for line in range(len(lines)):
-        for char in range(len(lines[line])):
-            if lines[line][char] == "S": return [char, line, "S"]
+for y in range(len(GRID)):
+    for x in range(len(GRID[y])):
+        if sym(x, y) == "S": path.append((x, y))
 
-def move(current):
-    dirs = []
-    match current[2]:
-        case "S": dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        case "-": dirs = [[1, 0], [-1, 0]]
-        case "|": dirs = [[0, 1], [0, -1]]
-        case "F": dirs = [[1, 0], [0, 1]]
-        case "J": dirs = [[-1, 0], [0, -1]]
-        case "L": dirs = [[1, 0], [0, -1]]
-        case "7": dirs = [[-1, 0], [0, 1]]
-    for dir in dirs:
-        if direction(current, dir):
-            return direction(current, dir)
-
-def direction(current, dir):
-    moves = {
-        "right": ["7", "J", "-", "S"],
-        "left": ["F", "L", "-", "S"],
-        "down": ["L", "J", "|", "S"],
-        "up": ["F", "7", "|", "S"]}
-
-    x, y = current[0], current[1]
-    match dir:
-        case [1, 0]:
-            pos = lines[y][x+1]
-            if pos in moves["right"] and not [x+1, y, pos] in path:
-                return [x+1, y, pos]
-        case [-1, 0]:
-            pos = lines[y][x-1]
-            if pos in moves["left"] and not [x-1, y, pos] in path:
-                return [x-1, y, pos]
-        case [0, 1]:
-            pos = lines[y+1][x]
-            if pos in moves["down"] and not [x, y+1, pos] in path:
-                return [x, y+1, pos]
-        case [0, -1]:
-            pos = lines[y-1][x]
-            if pos in moves["up"] and not [x, y-1, pos] in path:
-                return [x, y-1, pos]
-
-def getDots():
-    dots = []
-    for y in range(1, len(lines)-1):
-        for x in range(1, len(lines[y])-1):
-            if not (x, y) in path: dots.append((x, y))
-    return dots
-
-current = getStart()
-path.append(current)
 while True:
-    current = move(current)
-    if current == None: break
-    path.append(current)
+    step = move(path)
+    if step == None: break
+    path.append(step)
+path.append(path[0])
 
-for i in range(len(path)):
-    path[i] = (path[i][0], path[i][1])
+area = 0
+for p in range(len(path)-1):
+    area += path[p][0] * path[p+1][1]
+    area -= path[p][1] * path[p+1][0]
+area = (abs(area) + 1) / 2
+points = area - len(path) / 2 + 1
 
-dots = getDots()
-polygon = Polygon(path)
-points = [dot for dot in dots if polygon.contains(Point(dot))]
-print(len(points))
+print(points)
