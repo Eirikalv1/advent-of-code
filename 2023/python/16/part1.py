743c35a9188446d3bee297eaf6aeb40513de2a6b
Eirik <eirikalv@icloud.com>
2023-12-16 17:51:19 +0100

    day 16

---
+++ 2023/python/16/part1.py
@@ -0,0 +1,50 @@
+import numpy as np
+import sys
+
+sys.setrecursionlimit(5000)
+
+GRID = np.array(list(map(list, open("input.txt", "r").read().splitlines())))
+
+INTERACTIONS = {
+    ((0, 1), '-'): [(1, 0), (-1, 0)],
+    ((0, -1), '-'): [(1, 0), (-1, 0)],
+
+    ((1, 0), '|'): [(0, 1), (0, -1)],
+    ((-1, 0), '|'): [(0, 1), (0, -1)],
+
+    ((1, 0), '/'): [(0, -1)], ((-1, 0), '/'): [(0, 1)],
+    ((0, 1), '/'): [(-1, 0)], ((0, -1), '/'): [(1, 0)],
+
+    ((1, 0), '\\'): [(0, 1)], ((-1, 0), '\\'): [(0, -1)],
+    ((0, 1), '\\'): [(1, 0)], ((0, -1), '\\'): [(-1, 0)],
+}
+
+out_of_bounds = lambda pos: not 0 <= pos[0] < len(GRID[0]) or not 0 <= pos[1] < len(GRID)
+
+path, seen = set(), []
+
+def move(pos, aim):
+    if out_of_bounds(pos): return
+    if (pos, aim) in seen: return
+
+    interaction = (aim, GRID[pos[1], pos[0]])
+
+    path.add(pos)
+    seen.append((pos, aim))
+
+    if not interaction in INTERACTIONS:
+        move((pos[0] + aim[0], pos[1] + aim[1]), aim)
+        return
+    
+    for split_aim in INTERACTIONS[interaction]:
+        split_pos = (pos[0] + split_aim[0], pos[1] + split_aim[1])    
+        if out_of_bounds(split_pos): continue
+
+        path.add(split_pos)
+        move(split_pos, split_aim)
+
+
+pos, aim = (0, 0), (1, 0)
+move(pos, aim)
+
+print(len(path))
