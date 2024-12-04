9d20dca9107004c7033de283556b88cbe9a67411
Eirik <eirikalv@icloud.com>
2024-12-04 08:49:04 +0100

    fix

--- 2024/4/part2.py
+++ 2024/4/part2.py
@@ -5,6 +5,5 @@ for yi, yv in enumerate(lines):
     for xi, xv in enumerate(yv):
         if 0 < yi < len(lines) - 1 and 0 < xi < len(lines[yi]) - 1 and yv[xi] == 'A':
             corners = lines[yi-1][xi-1] + lines[yi-1][xi+1] + lines[yi+1][xi+1] + lines[yi+1][xi-1]
-            if corners.count('M') == 2 and corners.count('S') == 2 and corners[0] != corners[2] and corners[1] != corners[3]:
-                count += 1
+            count += corners.count('M') == 2 and corners.count('S') == 2 and corners[0] != corners[2] and corners[1] != corners[3]
 print(count)
